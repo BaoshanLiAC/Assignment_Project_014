40db87a4e19f0ca8980dfc42815a80a03edb2ab6
Xingan <wang0778@algonquinlive.com>
2020-11-08 23:33:27 -0500

    Initial commit in ticketmaster branch

---
+++ app/src/main/java/com/ac/assignment_project_014/ticketmaster/TicketMasterEventDetails.java
@@ -0,0 +1,16 @@
+package com.ac.assignment_project_014.ticketmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ac.assignment_project_014.R;
+
+public class TicketMasterEventDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ticketmaster_event_details);
+    }
+}
