09329c4f957e2f33ebed2bdcd4c67fabd36c377a
BaoshanLiAC <li000694@algonquinlive.com>
2020-11-14 23:52:37 -0500

    Audio API first version.

--- app/src/main/java/com/ac/assignment_project_014/MainActivity.java
+++ app/src/main/java/com/ac/assignment_project_014/MainActivity.java
@@ -2,7 +2,9 @@ package com.ac.assignment_project_014;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //audioDatabase
+        Button btn_audio = findViewById(R.id.btn_audio);
+        Intent goToAduioIndex  = new Intent(this, com.ac.assignment_project_014.BaoshanLi.AudioIndexActivity.class);
+        btn_audio.setOnClickListener( click -> {
+
+            startActivity( goToAduioIndex );
+        });
+
+
+
     }
 }
