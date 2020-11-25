3f8282a3c88cad7236ed71dae3d01f86c47bad74
ren00038 <ren00038@algonquinlive.com>
2020-11-24 21:00:33 -0500

    minor changes

--- app/src/main/java/com/ac/assignment_project_014/recipe/EmptyActivity.java
+++ app/src/main/java/com/ac/assignment_project_014/recipe/EmptyActivity.java
@@ -5,18 +5,27 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ac.assignment_project_014.R;
-
+/**
+ * The empty class used for tablet display.
+ * It Accept the data that was passed from Fragment.
+ * @author chunyan ren
+ * reference: from InclassProject FragmentExample.java lines 47-54
+ */
 public class EmptyActivity extends AppCompatActivity {
-
+    /**
+     * Initialize the activity and get get the data that was passed from Fragment.
+     * @param savedInstanceState used to save the current states
+     *
+     * @author chunyan ren
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_activity_empty);
 
-        Bundle dataToPass = getIntent().getExtras(); //get the data that was passed from FragmentExample
-        //This is copied directly from FragmentExample.java lines 47-54
+        Bundle dataToPass = getIntent().getExtras();
         DetailFragment dFragment = new DetailFragment();
-        dFragment.setArguments( dataToPass ); //pass data to the the fragment
+        dFragment.setArguments( dataToPass );
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragmentLocation, dFragment)
